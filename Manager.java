6785cf6da95cc9a7c62881906f5df70a0ef2d714
SG master <1363028533@qq.com>
2017-02-16 02:42:44 +0800

    Add files via upload

---
+++ Manager.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class Manager {
+	final private Card[] whole;
+	private Card[] hide;
+	private Card[] show;
+	Manager(){
+		whole = new Card[108];
+		whole[0] = new Card(1, 1);
+	}
+}
+
+class Card {
+	private int rank;
+	private int color;
+	Card(int rank, int color){
+		this.rank = rank;
+		this.color = color;
+	}
+	card(int rank, int color){
+		this.rank = rank;
+		this.color = color;
+	}
+	boolean Greater(Card c){
+		return color==c.color?rank>c.rank:color>c.color;
+	}
+	boolean Lesser(Card c){
+		return color==c.color?rank<c.rank:color<c.color;
+	}
+	boolean Equal(Card c){
+		return color==c.color&&rank==c.rank;
+	}
+	public String toString(){
+		String ret = new String("");
+		switch(rank){
+		case 1:ret+="一";break;
+		case 2:ret+="二";break;
+		case 3:ret+="三";break;
+		case 4:ret+="四";break;
+		case 5:ret+="五";break;
+		case 6:ret+="六";break;
+		case 7:ret+="七";break;
+		case 8:ret+="八";break;
+		case 9:ret+="九";break;
+		}
+		switch(color){
+		case 1:ret+="万";break;
+		case 2:ret+="筒";break;
+		case 3:ret+="条";break;
+		}
+		return ret;
+	}
+	void Show(){
+		System.out.println(this.toString());
+	}
+}
---
+++ Table.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Table {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("请输入用户名：");
+		String name = s.nextLine();
+		System.out.println(name);
+	}
+}
